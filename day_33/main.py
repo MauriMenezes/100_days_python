caea31f152141e9f97eaff8bd12b76d8eb34e38e
Maurilio Menezes <mauriliosm25@gmail.com>
2023-02-11 02:29:33 -0300

    feat: checking if the ISS is near and if its night

---
+++ day_33/main.py
@@ -0,0 +1,42 @@
+import requests
+from datetime import datetime
+
+MY_LAT = -12.918651
+MY_LONG = -38.630948
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude >= MY_LAT + 5 and MY_LONG-5 <= iss_longitude >= MY_LONG+5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get(
+        "https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+if is_iss_overhead() and is_night():
+    ...
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
